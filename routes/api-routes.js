c4673159c49f86155ee2b0ad5e2897655657e5c9
jdlee022 <jdlee022@gmail.com>
2017-05-02 21:26:54 -0700

    initial file structure setup

---
+++ routes/api-routes.js
@@ -0,0 +1,10 @@
+var db = require("../models");
+
+module.exports = function(app) {
+//   //Example
+//   app.post("/api/authors", function(req, res) {
+//     db.Author.create(req.body).then(function(dbAuthor) {
+//       res.json(dbAuthor);
+//     });
+//   });
+};
