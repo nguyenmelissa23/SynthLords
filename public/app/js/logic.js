98472bba247783297d6b888b92cd2b719a1ddd68
nguyenmelissa23 <nguyen.melissa23@gmail.com>
2017-05-06 13:40:07 -0700

    Tony's files

--- public/app/js/logic.js
+++ public/app/js/logic.js
@@ -1,2 +1,13 @@
 //FRONT END JS
 
+$(".led-red").on("click", function(){
+    $(this).toggleClass("led-red-on");
+    if (this.getAttribute("value") == "off"){
+   this.setAttribute("value", "on");
+   console.log("On: " + this);
+    }
+    else if (this.getAttribute("value") == "on"){
+        this.setAttribute("value", "off");
+        console.log("Off: " + this);
+    }
+});
